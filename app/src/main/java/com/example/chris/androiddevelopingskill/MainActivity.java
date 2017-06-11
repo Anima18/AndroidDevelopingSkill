c590a72a3862d872f0e702600b7528bf7995a959
Anima18 <591151451@qq.com>
2017-06-11 09:48:02 +0800

    init project

---
+++ app/src/main/java/com/example/chris/androiddevelopingskill/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.chris.androiddevelopingskill;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.chris.androiddevelopingskill.entity.ActivityClass;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private List<ActivityClass> activityClassList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initData();
+        initView();
+        initEvent();
+    }
+
+    public void initView() {
+        setTitle("MaterialDesignDemo");
+
+        recyclerView = (RecyclerView) findViewById(R.id.mainAct_data_rv);
+        MainAdapter adapter = new MainAdapter(this, activityClassList);
+        adapter.setOnItemClickListener(new MainAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View itemView, int position) {
+                ActivityClass activityClass = activityClassList.get(position);
+                if(activityClass.getActivityClass() == null) {
+                    Toast.makeText(MainActivity.this, "没有实现", Toast.LENGTH_SHORT).show();
+                }else {
+                    Intent intent = new Intent(MainActivity.this, activityClass.getActivityClass());
+                    startActivity(intent);
+                }
+            }
+        });
+        recyclerView.setAdapter(adapter);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, layoutManager.getOrientation()));
+        recyclerView.setLayoutManager(layoutManager);
+    }
+
+    public void initData() {
+        activityClassList = new ArrayList<>();
+
+        activityClassList.add(new ActivityClass("ListView", null));
+    }
+
+    public void initEvent() {
+
+    }
+}
